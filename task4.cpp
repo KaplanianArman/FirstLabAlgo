eb7c871f23f35fb10184569aefc80e3ae84bf713
Arman <kaplanianarman@gmail.com>
2026-09-09 21:08:27 +0300

    task4 commit

---
+++ task4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n; std::cin >> n;
+    std::vector<int> mas(n);
+    for (int i = 0; i < n; ++i) std::cin >> mas[i];
+    int x; std::cin >> x;
+    int mn = 1001, ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int cur = abs(mas[i] - x);
+        if (cur < mn) {
+            mn = cur;
+            ans = mas[i];
+        }
+    }
+    std::cout << ans << std::endl;
+    return 0;
+}
