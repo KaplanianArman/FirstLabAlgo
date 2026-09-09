bae70fd01567945ce4488a67e9db10ab54533637
Arman <kaplanianarman@gmail.com>
2026-09-09 21:27:01 +0300

    fixed task4

--- task4.cpp
+++ task4.cpp
@@ -6,7 +6,7 @@ int main() {
     std::vector<int> mas(n);
     for (int i = 0; i < n; ++i) std::cin >> mas[i];
     int x; std::cin >> x;
-    int mn = 1001, ans = 0;
+    int mn = 2001, ans = 0;
     for (int i = 0; i < n; ++i) {
         int cur = abs(mas[i] - x);
         if (cur < mn) {
